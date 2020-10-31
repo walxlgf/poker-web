a559bd9fc4c9de9997a67e12d1b881d257e3ac7d
walxlgf <guanghui-li@163.com>
2020-10-31 18:32:35 +0800

    cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,6 +7,8 @@ import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+import SeriesIndexPreview from './preview-templates/SeriesIndexPreview'
+
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
@@ -14,3 +16,5 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+CMS.registerPreviewTemplate('seriesIndex', SeriesIndexPreview)
