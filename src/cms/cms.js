8f90dd18feade64473a284c6fbdc094edacb7153
walxlgf <guanghui-li@163.com>
2020-10-31 18:55:43 +0800

    fdf

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -17,4 +17,4 @@ CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 
-CMS.registerPreviewTemplate('series-index', SeriesIndexPreview)
+CMS.registerPreviewTemplate('seriesindex', SeriesIndexPreview)
