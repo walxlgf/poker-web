c1195d381920087934f5e996866756b3978d935a
walxlgf <guanghui-li@163.com>
2020-11-01 16:42:58 +0800

    relatedSeries

--- src/templates/idx-page.js
+++ src/templates/idx-page.js
@@ -23,7 +23,17 @@ export const IdxPageTemplate = ({
         >
           {title}
         </h2>
-        <h2>{relatedSeries}</h2>
+        <h2
+          className="has-text-weight-bold is-size-1"
+          style={{
+            boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+            backgroundColor: '#f40',
+            color: 'white',
+            padding: '1rem',
+          }}
+        >
+          {relatedSeries}
+        </h2>
       </div>
       <div>
         {seriesBanner.map((series) => (
