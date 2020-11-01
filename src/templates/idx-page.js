7fa802cbce3fa0c477bb24578df2bcbb8d922477
walxlgf <guanghui-li@163.com>
2020-11-01 16:50:29 +0800

    h

--- src/templates/idx-page.js
+++ src/templates/idx-page.js
@@ -21,17 +21,7 @@ export const IdxPageTemplate = ({
             padding: '1rem',
           }}
         >
-          {title}
-        </h2>
-        <h2
-          className="has-text-weight-bold is-size-1"
-          style={{
-            boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-            backgroundColor: '#f40',
-            color: 'white',
-            padding: '1rem',
-          }}
-        >
+          {title} 
           {relatedSeries}
         </h2>
       </div>
